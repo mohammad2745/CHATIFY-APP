8d36761cded99020e9bccb41f1da1ccdc1e1c869
mohammad2745 <Mohammad.Sultan@mindstaq.com>
2026-04-13 16:11:33 +0600

    JSON limit Updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json("limit", "5mb"));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
